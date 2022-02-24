2ec2b5c38492dd956cbcaf046632da665b0f2608
Abdou <sene78@hotmail.com>
2022-02-23 20:20:48 -0500

    1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import Account from './Account';
+import Auth from './Auth';
+import { supabase } from "./supabaseClient";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Home() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    })
+  }, []);
+
+  return <ChakraProvider>{!session ? <Auth /> : <Account key={session.user.id} session={session} />}</ChakraProvider>
+
 }
-
-export default App;
